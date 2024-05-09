2b4ce18824b7d4cde730c9e405a5617d75d73d16
parthlr <parthlr321@gmail.com>
2024-05-08 22:50:07 -0400

    Added show message every few seconds test

--- extension.js
+++ extension.js
@@ -9,22 +9,10 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "motifications" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('motifications.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+	const showMessage = function () {
 		vscode.window.showInformationMessage('Hello World from Motifications!');
-	});
-
-	context.subscriptions.push(disposable);
+	 };
+	 setInterval(showMessage, 5000);
 }
 
 // This method is called when your extension is deactivated
